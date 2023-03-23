41afc477d869d855295291caabb12147e190b94e
Rahul <102817224+rahulsalgo@users.noreply.github.com>
2023-03-23 12:32:51 +0530

    this is the repo regarding the loops usage in the js

---
+++ script.js
@@ -0,0 +1,16 @@
+const num=[1,2,3,4,5,6,7,8,10,10]
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i])
+// }
+
+//for(let i in num)
+//{
+    // console.log(i)  this line will print the addresss of the numbers in array
+  //  console.log(num[i]) // this line will print the values of the arry
+//}
+
+for(let i of num)
+{
+   // console.log(i)// this line will give the output of the numbers in the arays
+    console.log(num[i])
+}
